7fcfe390e3cde2e18d49b7c29e4ab462ab83de17
Cyrus Sutaria <csutaria@users.noreply.github.com>
2020-04-10 18:07:14 -0700

    Added 'pyenv init' to activate pyenv, added comments

--- bootstrap.bash
+++ bootstrap.bash
@@ -1,10 +1,22 @@
 #! /usr/bin/env bash
 
+# Set up homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 brew update
+
+# Install pyenv
 brew install pyenv
+eval "$(pyenv init -)"
+
+# Set up a new python installation
 pyenv install 3.8.2
-pyenv global 3.8.2
+pyenv local 3.8.2
+pip install --upgrade pip
+
+# Create a virtualenv to install ansible to
 python -m venv venv
 source venv/bin/activate
+pip install --upgrade pip
 pip install ansible
+
+./playbook.yaml
